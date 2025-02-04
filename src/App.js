994eb925857f04e0849182410b8923fdbbde49fe
Cavan <cavanschutte@gmail.com>
2025-02-05 03:36:49 +0900

    Improvements

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import React from 'react';
 import Clock from './Clock';
 
 const App = () => (
-    <div
-        style={{
-            display: 'flex',
-            height: '100vh',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}
-    >
-        <Clock/>
-    </div>
+  <div className="h-screen w-screen flex justify-center items-center">
+
+      <div className='flex flex-col h-full w-full items-center justify-around'>
+
+          <h1 className='text-[5rem] text-center'>
+              24 Hour Clock
+          </h1>
+
+          <Clock />
+
+      </div>
+
+
+  </div>
 );
 
 export default App;
